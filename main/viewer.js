fcc7dd80ffa58cb6d1bd4cab38e4cdbffcf2a97e
eight <eight04@gmail.com>
2016-04-30 02:26:10 +0800

    Add status bar

--- main/viewer.js
+++ main/viewer.js
@@ -46,7 +46,7 @@ function createViewer() {
             
             var result = bbsReader(this.data);
                 
-            result = "<!DOCTPYE><html><head><title>" + (result.title ? result.title : "ANSI Viewer") + "</title><script src='" + module.uri + "/../../node_modules/pmore/dist/pmore.js'></script><link rel='stylesheet' href='" + module.uri + "/../../node_modules/bbs-reader/bbs-reader.css'><link rel='stylesheet' href='" + module.uri + "/../../public/viewer.css'><script src='" + module.uri + "/../../public/viewer.js'></script></head><body>" + result.html + "</body></html>";
+            result = "<!DOCTPYE><html><head><title>" + (result.title ? result.title : "ANSI Viewer") + "</title><script src='" + module.uri + "/../../node_modules/pmore/dist/pmore.js'></script><link rel='stylesheet' href='" + module.uri + "/../../node_modules/bbs-reader/bbs-reader.css'><link rel='stylesheet' href='" + module.uri + "/../../public/viewer.css'><script src='" + module.uri + "/../../public/viewer.js'></script></head><body><div class='bbs'>" + result.html + "</div><div class='statusbar'></div></body></html>";
             
             result = uao.decode(result);
             
